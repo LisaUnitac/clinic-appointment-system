c2c104e309fca9a0d00bf8e2fae63a051816eeea
LisaDyosi <lisakhanyadyosi03@gmail.com>
2025-08-13 11:38:43 +0200

    Package changes and fixed crud conflicts

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -22,6 +22,29 @@ exports.createAppointment = async (req, res) => {
     }
 };
 
+exports.updateAppointment = async (req, res) => {
+  const { id } = req.params;
+  const { user_id, doctor_id, appointment_date, updatedBy } = req.body;
+
+  try {
+    const [result] = await db.query(
+      `UPDATE appointments 
+       SET user_id = ?, doctor_id = ?, appointment_date = ?, updatedDate = NOW(), updatedBy = ?
+       WHERE id = ? AND isActive = 1`,
+      [user_id, doctor_id, appointment_date, updatedBy, id]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ error: "Appointment not found or inactive" });
+    }
+
+    res.json({ message: "Appointment updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
 exports.softDeleteAppointment = async (req, res) => {
     const { id } = req.params;
     try {
--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -4,6 +4,7 @@ const appointmentController = require("../controllers/appointmentController");
 
 router.get("/", appointmentController.getAppointments);
 router.post("/", appointmentController.createAppointment);
+router.put("/:id", appointmentController.updateAppointment)
 router.put("/soft-delete/:id", appointmentController.softDeleteAppointment);
 
 module.exports = router;
